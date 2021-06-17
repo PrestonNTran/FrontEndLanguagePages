53ac8bcba3664b1e93637b11dd95e199323ce079
Kyle Hekkers <kyle.hekkers@gmail.com>
2021-06-17 13:57:48 -0500

    JavaScript and Bootstrap

---
+++ FrontEndPages/FrontEndPages/Kyle/script.js
@@ -0,0 +1,1 @@
+﻿function showHide(id) {    var x = document.getElementById(id);    if (x.style.display !== "none") {        x.style.display = "none";    }    else {        x.style.display = "block";    }}
