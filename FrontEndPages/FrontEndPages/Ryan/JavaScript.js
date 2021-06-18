a1aaaaed243978df4cb7cf8402a180cab6490d0c
Ryan Flatley <22flatlr@elmbrookstudents.org>
2021-06-17 21:44:05 -0500

    added another

---
+++ FrontEndPages/FrontEndPages/Ryan/JavaScript.js
@@ -0,0 +1,1 @@
+﻿function showHide() {    var x = document.getElementById("mission");    if (x.style.display !== "none") {        x.style.display = "none";    }    else {        x.style.display = "block";    }  }
